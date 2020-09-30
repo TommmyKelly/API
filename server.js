e2f513eddbbefb5d29d7d4111e23c97ec5d3efeb
TommmyKelly <tommy_kelly@icloud.com>
2020-09-30 13:51:09 +0100

    ADD: added mongo DB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+
 
 //Route files
 const bootcamps = require('./routes/bootcamps.js')
@@ -8,6 +10,10 @@ const bootcamps = require('./routes/bootcamps.js')
 
 dotenv.config({ path: './config/config.env'})
 
+//connect to database
+
+connectDB();
+ 
 const app = express();
 
 // Dev logging middleware
@@ -18,10 +24,17 @@ if(process.env.NODE_ENV === 'development'){
 
 //mount routers
 
-app.use('/api/v1/bootcamps',bootcamps);
+app.use('/api/v1/bootcamps',bootcamps); 
 
 
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)) 
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)) 
+
+// Handle unhandled rejection ot unhandeled promises
+process.on('unhandledRejection',(err, promise) =>{
+   console.log(`Error: ${err.message}`)
+   // CLose server & exit process
+   server.close(()=>process.exit(1));
+})
