5b321c4720dd4831eb1cf55387d373143f5950be
TommmyKelly <tommy_kelly@icloud.com>
2020-10-14 18:05:37 +0100

    ADD: Column select and column to DB

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,19 +2,45 @@ const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp');
 
 const ErrorResponse = require('../utils/errorResponse')
-const asyncHandler = require('../middleware/async')
+const asyncHandler = require('../middleware/async');
+
 // @desc  Get all bootdcamps 
 //@route  Get /api/v1/bootcamps
 //@access Public
 exports.getBootcamps = asyncHandler(async (req, res, next)=>{
         let query;
 
-        let queryStr = JSON.stringify(req.query)
+        //copy req.query
+        const reqQuery = { ...req.query }
+
+        //Fields to exclude
+        const removeFields= ['select','sort'];
+
+        //loop over removeFields and delete form requesy query
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        console.log(reqQuery)
+
+        //Create quey string
+        let queryStr = JSON.stringify(reqQuery)
 
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
         console.log(queryStr)
 
         query = Bootcamp.find(JSON.parse(queryStr))
+
+        //select fields
+        if(req.query.select){
+            const fields = req.query.select.split(',').join(' ')
+            query = query.select(fields)
+        }
+        //sort fields
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        }else {
+            query = query.sort('-createdAt')
+        }
 
        
 
