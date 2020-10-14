c04848c68eb027c33cd19c51fe834ecfa107bf31
TommmyKelly <tommy_kelly@icloud.com>
2020-10-14 18:29:56 +0100

    ADD: pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -44,16 +44,35 @@ exports.getBootcamps = asyncHandler(async (req, res, next)=>{
 
         //Pagination
         const page = parseInt(req.query.page, 10) || 1;
-        const limit = parseInt(req.query.limit,10) || 100;
-        const skip = (page -1) * limit
+        const limit = parseInt(req.query.limit,10) || 25;
+        const startIndex = (page -1) * limit
+        const endIndex = page * limit
+        const total = await Bootcamp.countDocuments()
 
-        query = query.skip(skip).limit(limit);
+
+        query = query.skip(startIndex).limit(limit);
        
         //Excuting query
         const bootcamps = await query
+        //Pagination
+        const pagination = {};
+        if(endIndex < total){
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }
+
+        if(startIndex > 0){
+            pagination.prev ={
+               page: page - 1,
+               limit 
+            }
+        }
         res.status(200).json({
             success: true,
             count:bootcamps.length,
+            pagination,
             data: bootcamps
 
         })
